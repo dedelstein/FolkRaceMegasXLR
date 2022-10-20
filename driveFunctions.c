42a9b4f151ec476890db5e192c6b3285367fb054
Dan <danhedelstein@gmail.com>
2022-10-20 22:13:04 +0300

    slightly less stupid

--- driveFunctions.c
+++ driveFunctions.c
@@ -1,9 +1,9 @@
 #include <math.h>
 #include <stdbool.h>
 
-#define WIDTH 15
-#define MAGICNUMBER 1
-#define SPEEDMULT 10 // To scale from target vector magnitude to relevant # range for PWM
+#define WIDTH .15 // Width of current model
+#define MAGICNUMBER 1 // There are several factors such as wheel slippage and inertia that can collapse to a constant
+#define MAXSPEED 100 // TBD experimentally
 
 void MoveLeftWheel(bool dir, int speed) {
   	HAL_GPIO_WritePin(L_MOTOR_DIR_GPIO_Port, L_MOTOR_DIR_Pin, dir);
@@ -38,18 +38,20 @@ void Go(int *moveVector, int currSpeed) {
 
 	  double turnAngle = angleDiff(moveVector, currSpeed);
 
-	  int steerVect[2] = { moveVector[0], moveVector[1] - currSpeed }; // target vector
+	  //int steerVect[2] = { moveVector[0], moveVector[1] - currSpeed }; // target vector
 
 	  float speedRatio = fabs(turnAngle) * 2 * WIDTH * fabs(sin(turnAngle/2)) * MAGICNUMBER; // this is maybe very wrong
 
-	  int speed = sqrt(steerVect[0]*steerVect[0] + steerVect[1]*steerVect[1]) * SPEEDMULT;
+	  // This is maybe stupid, setting speed based on target vector magnitude.
+	  // Should probably just always go to max functional speed
+	  //int speed = sqrt(steerVect[0]*steerVect[0] + steerVect[1]*steerVect[1]) * SPEEDMULT;
+	  int speed = MAXSPEED;
 
 	  // hardR/hardL are 0 unless true, if turn angle is > 0, L wheel should go faster by N, and the reverse for R
 
-	  int driveSpeedL = hardR * speed + hardL * speed + (!hardR && !hardL) * (speed + (turnAngle > 0) * speed * speedRatio);
-	  int driveSpeedR = hardR * speed + hardL * speed + (!hardR && !hardL) * (speed + (turnAngle < 0) * speed * speedRatio);
+	  int driveSpeedL = hardR * speed + hardL * speed + (!hardR && !hardL) * (speed - (turnAngle < 0) * speed * speedRatio);
+	  int driveSpeedR = hardR * speed + hardL * speed + (!hardR && !hardL) * (speed - (turnAngle > 0) * speed * speedRatio);
 
-	  MoveLeftWheel(dirL, (int) driveSpeedL);
-	  MoveRightWheel(dirR, (int) driveSpeedR);
-
+	  MoveLeftWheel(dirL, driveSpeedL);
+	  MoveRightWheel(dirR, driveSpeedR);
 }
